194e1ce932beeef8495953b0dbf5a5e5d2a93113
Debora R <deborarebelatto1@gmail.com>
2020-04-30 17:20:27 -0300

    final commit

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,8 @@
-import React from "react"
+import React from 'react'
 
 function Header() {
-  return (
+  return(
     <header>
-      <img
-        src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png"
-        alt="Problem?"
-      />
       <p>Meme Generator</p>
     </header>
   )
